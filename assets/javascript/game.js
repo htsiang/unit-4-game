08f0d5b534e193cf571e0d85108f6ef08f8a50b7
htsiang <htsiang3@gmail.com>
2019-04-02 23:22:41 -0700

    Fleshed out html code. Coded basic functionality of game in javascript file. Have not coded css for how the game should look yet.

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+$(document).ready( function() {
+
+var goalScore = 0; // score that player is trying to match
+var playerScore = 0; // player's score that increments each time a rupee is clicked
+var winCount = 0; // track the # of wins
+var lossCount = 0; // track the # of losses
+
+newGame();
+
+$(".rupee-div").click(function () {
+    console.log($(this).attr("value"));
+
+    // increment playerScore by the rupee value chosen
+    playerScore += parseInt($(this).attr("value"));
+    console.log("player's score: " + playerScore);
+
+    if (goalScore===playerScore) {
+        winCount++;
+        console.log("wins: " + winCount);
+        $("#wins").text(winCount);
+        newGame();
+    }
+    else if (playerScore>goalScore) {
+        lossCount++;
+        console.log("losses: " + lossCount);
+        $("#losses").text(lossCount);
+        newGame();
+    };
+    
+});
+
+
+
+function newGame() {
+    console.log("New Game");
+    // reset goal score & player's score
+    goalScore = Math.floor(Math.random()*101)+19
+    console.log("goalScore: " + goalScore);
+    playerScore = 0;
+    console.log("player's score: " + playerScore);
+
+    // assign random number between 1-12 to each rupee
+    $("#rupee1").attr("value", Math.floor(Math.random()*12)+1);
+    $("#rupee2").attr("value", Math.floor(Math.random()*12)+1);
+    $("#rupee3").attr("value", Math.floor(Math.random()*12)+1);
+    $("#rupee4").attr("value", Math.floor(Math.random()*12)+1);
+
+    console.log("rupee1: " + $("#rupee1").attr("value"));
+    console.log("rupee2: " + $("#rupee2").attr("value"));
+    console.log("rupee3: " + $("#rupee3").attr("value"));
+    console.log("rupee4: " + $("#rupee4").attr("value"));
+
+    // show goalScore on html page
+    $("#goal-num").text(goalScore);
+}
+
+});
+
